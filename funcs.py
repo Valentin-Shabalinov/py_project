501585fbfe265bec80b00f9dd97530ec74786b06
Valentin-Shabalinov <111199259+Valentin-Shabalinov@users.noreply.github.com>
2023-07-15 11:56:43 +0300

    Add files via upload

---
+++ funcs.py
@@ -0,0 +1,8 @@
+def fold(a, b):
+    c = a + b
+    print(c)
+
+
+def subtraction(a, b):
+    c = a - b
+    return c
---
+++ main.py
@@ -0,0 +1,6 @@
+from funcs import fold, subtraction
+
+
+fold(2, 4)
+
+subtraction(5, 7)
